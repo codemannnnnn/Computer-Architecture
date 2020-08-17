f99ffb7f4ef6b478201707d2f7f0c5490d405e7b
Cody Branson <codyboi7000@gmail.com>
2020-08-17 17:04:35 -0600

    new updates

---
+++ ls8/class_notes/challenge.py
@@ -0,0 +1,16 @@
+# Implement functionality to reverse an input string. Print out the reversed string.
+# For example, given a string "cool", print out the string "looc".
+# You may use whatever programming language you'd like.
+# Verbalize your thought process as much as possible before writing any code.
+# Run through the UPER problem solving framework while going through your thought process.
+#
+#
+
+
+
+def reverse(word):
+    print(word[::-1])
+
+
+
+reverse('ydoc')
