c1b8eebefc63db9e869ed84ae1361a4396ef9c26
Cody Branson <codyboi7000@gmail.com>
2020-08-19 16:44:07 -0600

    new updates

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,38 +7,48 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 255
+        self.ram = [0] * 256
         self.reg = [0] * 8
-
+        self.pc = 0
 
     def load(self):
-        """Load a program into memory."""
+        """Load a program into ram."""
 
+        file = sys.argv[1]
         address = 0
 
-        # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        with open(file) as f:
+            for line in f:
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                line = line.split("#")[0].strip()
+
+                if line == "":
+                    continue
+
+                else:
+                    self.ram[address] = int(line, 2)
+                    address += 1
+
+                print(line)
+
 
 
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+        """ALU / Math operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+
+        elif op == "DIV":
+            self.reg[reg_a] /= self.reg[reg_b]
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -64,4 +74,39 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+        MUL = 0b10100010
+
+        running = True
+
+        while running:
+            ir = self.ram[self.pc]
+
+            reg_a = self.ram[self.pc + 1]
+            reg_b = self.ram[self.pc + 2]
+
+            if ir == HLT:
+                self.running = False
+                self.pc += 1
+
+            elif ir == LDI:
+                self.reg[reg_a] = reg_b
+                self.pc += 3
+
+            elif ir == PRN:
+                print(self.reg[reg_a])
+                self.pc += 2
+
+            elif ir == MUL:
+                self.reg[reg_a] *= self.reg[reg_b]
+                self.pc += 3
+
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
